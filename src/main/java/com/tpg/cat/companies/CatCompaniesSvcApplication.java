a2605f349f55cb1ef272ef60ead5e31446046f4e
Tony <tpg@blueyonder.co.uk>
2021-08-26 21:06:21 +0100

    Project initialised.

---
+++ src/main/java/com/tpg/cat/companies/CatCompaniesSvcApplication.java
@@ -0,0 +1,13 @@
+package com.tpg.cat.companies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatCompaniesSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatCompaniesSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpg/cat/companies/CatCompaniesSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpg.cat.companies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatCompaniesSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
