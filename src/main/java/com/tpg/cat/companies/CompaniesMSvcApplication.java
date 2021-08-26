476f950801c444e913493e2392289963c3c596ea
Tony <tpg@blueyonder.co.uk>
2021-08-26 21:08:29 +0100

    renamed main application class

--- src/main/java/com/tpg/cat/companies/CompaniesMSvcApplication.java
+++ src/main/java/com/tpg/cat/companies/CompaniesMSvcApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatCompaniesSvcApplication {
+public class CompaniesMSvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatCompaniesSvcApplication.class, args);
+		SpringApplication.run(CompaniesMSvcApplication.class, args);
 	}
-
 }
--- src/test/java/com/tpg/cat/companies/CompaniesMSvcApplicationTests.java
+++ src/test/java/com/tpg/cat/companies/CompaniesMSvcApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CatCompaniesSvcApplicationTests {
+class CompaniesMSvcApplicationTests {
 
 	@Test
 	void contextLoads() {
